7f30c63006a168ada68597af9af2b933f2440632
roiweb <info@roiweb.co>
2017-08-17 08:29:18 +0200

    Check if woocommerce is active

--- woobizz-list.php
+++ woobizz-list.php
@@ -16,15 +16,32 @@ add_action( 'plugins_loaded', 'woobizz_list_load_textdomain' );
 function woobizz_list_load_textdomain() {
   load_plugin_textdomain( 'woobizz-list', false, basename( dirname( __FILE__ ) ) . '/lang' ); 
 }
-// The prefix Name
-define('WOOBIZZLIST', 'woobizzlist');
-// Include the Whitelabel plugin
-include('admin/list-admin-page.php' );
-// Include the $options array 
-include( 'admin/list-options.php' );
-// Include all sections php files
-foreach ( glob( plugin_dir_path( __FILE__ ) . "/admin/sections/*.php" ) as $file ) {
-    include_once $file;
+//Check if WooCommerce is active
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	//echo "woocommerce is active";
+    // Add the prefix Name
+	define('WOOBIZZLIST', 'woobizzlist');
+	// Include the Whitelabel plugin
+	include('admin/list-admin-page.php' );
+	// Include the $options array 
+	include( 'admin/list-options.php' );
+	// Include all sections php files
+	foreach ( glob( plugin_dir_path( __FILE__ ) . "/admin/sections/*.php" ) as $file ) {
+		include_once $file;
+	}
+	// Add options and menus
+	$options_page = new WoobizzListOptions( 'WooBizz List', 'woobizz-list', WOOBIZZLIST, 'woobizz', null, 'read', null, true, false, true, $options );
+}else{
+	//Show message on admin
+	//echo "woocommerce is not active";
+	add_action( 'admin_notices', 'woobizz_list_admin_notice' );
+	
 }
-// ADD OPTIONS AND SUBMENU
-$options_page = new WoobizzListOptions( 'WooBizz List', 'woobizz-list', WOOBIZZLIST, 'woobizz', null, 'read', null, true, false, true, $options );
+//Woobizz List Notice
+function woobizz_list_admin_notice() {
+    ?>
+    <div class="notice notice-error is-dismissible">
+        <p><?php _e( 'Woobizz list needs woocommerce to work properly, please install and activate woocommerce or disable this plugin.', 'woobizz-list' ); ?></p>
+    </div>
+    <?php
+}
